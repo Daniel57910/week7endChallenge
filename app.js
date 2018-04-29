3ac74802b5fb91c889cc61e349f6d6739918092e
Daniel Miller <danielmiller5791@gmail.com>
2018-04-29 10:26:34 +0100

    CRUD terminal program created, bowling now

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'w6_test';
+let LoginInfo = require('./controllers/loginController.js');
+const app = express();
+
+function connectToDatabase() {
+  MongoClient.connect(url, function (err, client) {
+    const db = client.db(dbName);
+    const collection = db.collection('test_collection');
+    client.close();
+  });
+}
+
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.send('test routing')
+});
+
+
+
+app.listen(9000, () => console.log('Example app listening on port 9000!'));
---
+++ controllers/loginController.js
@@ -0,0 +1,7 @@
+let LoginInfo = {};
+
+LoginInfo.name = "Rob";
+LoginInfo.email = "test@gmail.com";
+LoginInfo.password = "123456";
+
+module.exports = LoginInfo;
